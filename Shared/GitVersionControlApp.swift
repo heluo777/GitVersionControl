c2a82bc6bcbb16c68aaeeb8af258502646fdecbd
heluo777 <macs_he@icloud.com>
2021-02-01 15:37:41 +0800

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by mac on 2021/2/1.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
